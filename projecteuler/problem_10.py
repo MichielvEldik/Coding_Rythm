3b9efc3239ba92a17d991478dde239dafb9eb5cd
Mitchy123 <michiel_eldik@msn.com>
2020-08-25 23:28:04 +0200

    cleaned up some stuff.

--- projecteuler/problem_10.py
+++ projecteuler/problem_10.py
@@ -1,6 +1,9 @@
 """
-Find sum of all primes below 2,000,000
+Find sum of all primes below 2,000,000.
+Two alternative ways to implement the Sieve of Eratosthenes algorithm and compute the answer.
+
 """
+import math
 
 
 def sieve(number):
@@ -22,3 +25,22 @@ def sieve(number):
 
 NUMBER = 2_000_000
 sieve(NUMBER)
+
+
+def sieve_alt(limit):
+    max_number_to_be_checked = int(math.sqrt(limit))
+    nummies = [x for x in range(2, limit + 1)]
+    for n in nummies:
+        if n == -1:
+            continue
+        if n > max_number_to_be_checked:
+            break
+        m = n
+        for x in range(m + m - 2, len(nummies), m):  # m + m - 2 = INDEX 4, equivalent to #6 because nummies starts at 2
+            nummies[x] = -1
+    return [i for i in nummies if i != -1]
+
+
+primes = sieve_alt(2_000_000)
+print(sum(primes))
+
