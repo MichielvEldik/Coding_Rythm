24bf6792b1b01fd4c56e72e61570eea6dad7d44e
Mitchy123 <michiel_eldik@msn.com>
2020-08-26 15:56:27 +0200

    before I mess everything up

--- projecteuler/Problem_11.py
+++ projecteuler/Problem_11.py
@@ -11,26 +11,27 @@ matrix_slice = [[3, 8, 7, 9],
                 [4, 1, 3, 2]]
 
 
+def funky(matrix_slice):
+    """
+    :param matrix_slice: a slice of the matrix len = number of adjacent things (4)
+    :return: product list.
+    """
 
+    x = lambda i: matrix_slice[0][i] * matrix_slice[1][i + 1]
+    x_reverse = lambda o: matrix_slice[0][o] * matrix_slice[1][o - 1]
+    product_list = []
 
+    for b in range(0, 3):
+        print(x(b))
+        product_list.append(x(b))
 
+    for c in range(3, 0, -1):
+        print(x_reverse(c))
+        product_list.append(x_reverse(c))
 
-x = lambda i: matrix_slice[0][i] * matrix_slice[1][i+1]
-x_reverse = lambda o: matrix_slice[0][o] * matrix_slice[1][o-1]
+    return product_list
 
-
-
-
-product_list =[]
-for b in range(0, 3):
-    print(x(b))
-    product_list.append(x(b))
-
-for c in range(3, 0, -1):
-    print(x_reverse(c))
-    product_list.append(x_reverse(c))
-
-print(product_list)
+print(funky(matrix_slice))
 
 
 
